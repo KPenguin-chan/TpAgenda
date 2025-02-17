5cf1563296d24504e670b53582a9e4e30ec3af07
Julie Leroy <22301676@me.uvsq.fr>
2025-02-17 10:10:07 +0100

    correction commentaire

--- src/test/java/modele/PlanningCollectionsTest.java
+++ src/test/java/modele/PlanningCollectionsTest.java
@@ -24,11 +24,11 @@ class PlanningCollectionsTest {
         expectedPlanning.ajout(r2);
         expectedPlanning.ajout(r1);
 
-        /*
-        2)Les différences qu'on peut voir sont que la liste n'est pas limiter en taille
-        donc pas besoin de tester l'ajout en cas de liste pleine et la fonction ajout
-        de projet émet une exception pour une date fausse
-        */
+
+        //2)Les différences qu'on peut voir sont que la liste n'est pas limiter en taille
+        //donc pas besoin de tester l'ajout en cas de liste pleine et la fonction ajout
+        //de projet émet une exception pour une date fausse
+
 
 
         assertTrue(expectedPlanning.compareTo(p1));
@@ -37,12 +37,12 @@ class PlanningCollectionsTest {
     @Test
     void getReservations() throws ExceptionPlanning {
 
-        /*
-        2)Les différences qu'on peut voir sont le type de la liste qui est un Treeset qui fait que la manière de parcourir est différente,7
-            Il y a également un test dans le cas d'une liste vide qui permet de renvoyer null. Ces changments n'affecte pas les test à faire
-            car c'est la même méthode dans l'ensemble.
 
-        */
+        //2)Les différences qu'on peut voir sont le type de la liste qui est un Treeset qui fait que la manière de parcourir est différente,7
+        //Il y a également un test dans le cas d'une liste vide qui permet de renvoyer null. Ces changments n'affecte pas les test à faire
+        //car c'est la même méthode dans l'ensemble.
+
+
 
         PlageHoraire ph1 = new PlageHoraire(new Horaire(20,30),new Horaire(23,15));
         PlageHoraire ph2 = new PlageHoraire(new Horaire(10,30),new Horaire(15,15));
@@ -70,12 +70,11 @@ class PlanningCollectionsTest {
         assertNull(p2.getReservations(r4.getDate()));
     }
 
-/* 2)La méthode plusAncienneReservation n'existe pas dans PlanningCollections mais si elle devait exister au sein
-* de planningCollections elle ne servirait pas à grand chose car l'indice de la plus Ancienne Réservation est 0 étant donné
-* que les réservation sont stockés dans un TreeSet
-*
-*
-* 2)La méthode Tri n'existe pas non plus car les réservations sont dans un TreeSet.*/
+ //2)La méthode plusAncienneReservation n'existe pas dans PlanningCollections mais si elle devait exister au sein
+    // de planningCollections elle ne servirait pas à grand chose car l'indice de la plus Ancienne Réservation est 0 étant donné
+    // que les réservation sont stockés dans un TreeSet
+
+ //2)La méthode Tri n'existe pas non plus car les réservations sont dans un TreeSet.
 
 
 }
